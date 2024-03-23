4cc61d8d1339d46fb63044f060697dc8f0ad3cac
zhjch <bingyu27@126.com>
2024-03-24 00:58:04 +0800

    add full-dadbod

---
+++ lua/bundle_plugins/plugins/full-dadbod.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = "tpope/vim-dadbod",
+    cmd = "DBUIToggle",
+    keys = {
+      {
+        mode = { "n", "v" },
+        "<leader>td",
+        "<cmd>DBUIToggle<CR>",
+        desc = "ToggleTerm DBUI",
+      },
+    },
+    lazy = true,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = {
+      {
+        "kristijanhusak/vim-dadbod-completion",
+        init = function()
+          vim.api.nvim_create_autocmd("FileType", {
+            desc = "dadbod completion",
+            group = vim.api.nvim_create_augroup("dadbod_cmp", { clear = true }),
+            pattern = { "sql", "mysql", "plsql" },
+            callback = function() require("cmp").setup.buffer { sources = { { name = "vim-dadbod-completion" } } } end,
+          })
+        end,
+      },
+    },
+  },
+}
