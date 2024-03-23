89579b9a6bb3b4718ca68af2f39d5d2bed64d3cf
zhjch <bingyu27@126.com>
2024-03-23 20:07:52 +0800

    add code_runner

---
+++ lua/bundle_plugins/plugins/code_runner.lua
@@ -0,0 +1,21 @@
+local prefix = "<leader>r"
+-- local maps = { n = {} }
+-- local icon = vim.g.icons_enabled and "󰁕 " or ""
+-- maps.n[prefix] = { desc = icon .. "RunCode" }
+-- require("astronvim.utils").set_mappings(maps)
+
+return {
+	"CRAG666/code_runner.nvim",
+	event = "User AstroFile",
+	keys = {
+		{ mode = { "n", "v" }, "<leader>rf", "<cmd>RunFile<CR>", desc = "RunFile" },
+		{ mode = { "n", "v" }, "<leader>rc", "<cmd>wincmd j | RunClose<CR>", desc = "RunClose" },
+	},
+	opts = {
+		focus = false,
+		filetype = {
+			python = "python3 -u",
+			lua = "lua",
+		},
+	},
+}
