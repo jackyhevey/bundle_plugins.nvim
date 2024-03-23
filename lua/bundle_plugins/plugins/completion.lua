ab85c9c26da519fc458f291a5c461bfbef10c2d2
zhjch <bingyu27@126.com>
2024-03-24 00:45:47 +0800

    codeium to completion

--- lua/bundle_plugins/plugins/completion.lua
+++ lua/bundle_plugins/plugins/completion.lua
@@ -9,4 +9,21 @@ return {
     event = "InsertEnter",
     opts = { accept_keymap = "<C-e>" },
   },
+  {
+    "Exafunction/codeium.vim",
+    event = "InsertEnter",
+    config = function()
+      vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+      vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+      vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+      vim.keymap.set("n", "<Leader>;", function()
+        if vim.g.codeium_enabled == true then
+          vim.cmd "CodeiumDisable"
+        else
+          vim.cmd "CodeiumEnable"
+        end
+      end, { noremap = true, desc = "Toggle Codeium active" })
+    end,
+  },
 }
