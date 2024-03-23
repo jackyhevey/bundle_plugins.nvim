ddb246029d71d178e1df35545abe22921235dc84
zhjch <bingyu27@126.com>
2024-03-24 00:48:21 +0800

    marks to motion

--- lua/bundle_plugins/plugins/motion.lua
+++ lua/bundle_plugins/plugins/motion.lua
@@ -65,4 +65,19 @@ return {
       },
     },
   },
+  {
+    "chentoast/marks.nvim",
+    event = "BufEnter",
+    opts = {
+      excluded_filetypes = {
+        "qf",
+        "NvimTree",
+        "toggleterm",
+        "TelescopePrompt",
+        "alpha",
+        "netrw",
+        "neo-tree",
+      },
+    },
+  },
 }
