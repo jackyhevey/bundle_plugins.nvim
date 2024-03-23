3af85bf840a353a471d278cdf98de47707b1ced0
zhjch <bingyu27@126.com>
2024-03-23 17:05:22 +0800

    add harpoon

---
+++ lua/bundle_plugins/plugins/harpoon.lua
@@ -0,0 +1,60 @@
+local prefix = "<leader><leader>"
+local term_string = vim.fn.exists("$TMUX") == 1 and "tmux" or "term"
+return {
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		keys = {
+			{
+				prefix .. "a",
+				function()
+					require("harpoon"):list():append()
+				end,
+				desc = "Add file",
+			},
+			{
+				prefix .. "e",
+				function()
+					require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+				end,
+				desc = "Toggle quick menu",
+			},
+			{
+				prefix .. "x",
+				function()
+					vim.ui.input({ prompt = "Harpoon mark index: " }, function(input)
+						local num = tonumber(input)
+						if num then
+							require("harpoon"):list():select(num)
+						end
+					end)
+				end,
+				desc = "Goto index of mark",
+			},
+			-- { prefix .. "n", function() require("harpoon"):list():prev() end, desc = "Goto previous mark" },
+			-- { prefix .. "p", function() require("harpoon"):list():next() end, desc = "Goto next mark" },
+			{ prefix .. "m", "<cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" },
+			-- {
+			--   prefix .. "t",
+			--   function()
+			--     vim.ui.input({ prompt = term_string .. " window number: " }, function(input)
+			--       local num = tonumber(input)
+			--       if num then require("harpoon").term.gotoTerminal(num) end
+			--     end)
+			--   end,
+			--   desc = "Go to " .. term_string .. " window",
+			-- },
+		},
+		opts = {},
+	},
+	{
+		"catppuccin/nvim",
+		optional = true,
+		---@type CatppuccinOptions
+		opts = { integrations = { harpoon = true } },
+	},
+}
