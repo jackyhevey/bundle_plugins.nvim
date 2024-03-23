bcb9606d27bdb455bae9c6724821a79a8d25b5d1
zhjch <bingyu27@126.com>
2024-03-24 00:40:51 +0800

    split to motion

--- lua/bundle_plugins/plugins/enhance.lua
+++ lua/bundle_plugins/plugins/enhance.lua
@@ -26,72 +26,72 @@ return {
       }
     end,
   },
-  {
-    "echasnovski/mini.surround",
-    keys = function(_, keys)
-      -- local maps = { n = {} }
-      -- maps.n["gs"] = { desc = "Surround" }
-      -- require("astronvim.utils").set_mappings(maps)
-      -- Populate the keys based on the user's options
-      local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-      local mappings = {
-        { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
-        { opts.mappings.delete, desc = "Delete surrounding" },
-        { opts.mappings.find, desc = "Find right surrounding" },
-        { opts.mappings.find_left, desc = "Find left surrounding" },
-        { opts.mappings.highlight, desc = "Highlight surrounding" },
-        { opts.mappings.replace, desc = "Change surrounding" },
-        { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-      }
-      mappings = vim.tbl_filter(function(m) return m[1] and #m[1] > 0 end, mappings)
-      return vim.list_extend(mappings, keys)
-    end,
-    opts = {
-      mappings = {
-        add = "gsa", -- Add surrounding in Normal and Visual modes
-        delete = "gsd", -- Delete surrounding
-        find = "gsf", -- Find surrounding (to the right)
-        find_left = "gsF", -- Find surrounding (to the left)
-        highlight = "gsh", -- Highlight surrounding
-        replace = "gsc", -- change surrounding
-        update_n_lines = "gsn", -- Update `n_lines`
-      },
-    },
-  },
-  {
-    "folke/flash.nvim",
-    -- event = "VeryLazy",
-    opts = {
-      modes = {
-        char = {
-          jump_labels = true,
-        },
-      },
-    },
-    keys = {
-      { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
-      {
-        "S",
-        mode = { "n", "o", "x" },
-        function() require("flash").treesitter() end,
-        desc = "Flash Treesitter",
-      },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      {
-        "R",
-        mode = { "o", "x" },
-        function() require("flash").treesitter_search() end,
-        desc = "Treesitter Search",
-      },
-      {
-        "<c-s>",
-        mode = { "c" },
-        function() require("flash").toggle() end,
-        desc = "Toggle Flash Search",
-      },
-    },
-  },
+  -- {
+  --   "echasnovski/mini.surround",
+  --   keys = function(_, keys)
+  --     -- local maps = { n = {} }
+  --     -- maps.n["gs"] = { desc = "Surround" }
+  --     -- require("astronvim.utils").set_mappings(maps)
+  --     -- Populate the keys based on the user's options
+  --     local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
+  --     local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+  --     local mappings = {
+  --       { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
+  --       { opts.mappings.delete, desc = "Delete surrounding" },
+  --       { opts.mappings.find, desc = "Find right surrounding" },
+  --       { opts.mappings.find_left, desc = "Find left surrounding" },
+  --       { opts.mappings.highlight, desc = "Highlight surrounding" },
+  --       { opts.mappings.replace, desc = "Change surrounding" },
+  --       { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+  --     }
+  --     mappings = vim.tbl_filter(function(m) return m[1] and #m[1] > 0 end, mappings)
+  --     return vim.list_extend(mappings, keys)
+  --   end,
+  --   opts = {
+  --     mappings = {
+  --       add = "gsa", -- Add surrounding in Normal and Visual modes
+  --       delete = "gsd", -- Delete surrounding
+  --       find = "gsf", -- Find surrounding (to the right)
+  --       find_left = "gsF", -- Find surrounding (to the left)
+  --       highlight = "gsh", -- Highlight surrounding
+  --       replace = "gsc", -- change surrounding
+  --       update_n_lines = "gsn", -- Update `n_lines`
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "folke/flash.nvim",
+  --   -- event = "VeryLazy",
+  --   opts = {
+  --     modes = {
+  --       char = {
+  --         jump_labels = true,
+  --       },
+  --     },
+  --   },
+  --   keys = {
+  --     { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+  --     {
+  --       "S",
+  --       mode = { "n", "o", "x" },
+  --       function() require("flash").treesitter() end,
+  --       desc = "Flash Treesitter",
+  --     },
+  --     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+  --     {
+  --       "R",
+  --       mode = { "o", "x" },
+  --       function() require("flash").treesitter_search() end,
+  --       desc = "Treesitter Search",
+  --     },
+  --     {
+  --       "<c-s>",
+  --       mode = { "c" },
+  --       function() require("flash").toggle() end,
+  --       desc = "Toggle Flash Search",
+  --     },
+  --   },
+  -- },
   {
     "codota/tabnine-nvim",
     main = "tabnine",
