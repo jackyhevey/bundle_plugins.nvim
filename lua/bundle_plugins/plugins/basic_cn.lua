b20363e7590530abf53525e10ed3fb4496bd3987
zhjch <bingyu27@126.com>
2024-03-29 17:46:08 +0800

    add basic_cn

---
+++ lua/bundle_plugins/plugins/basic_cn.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "keaising/im-select.nvim",
+    main = "im_select",
+    build = '/bin/bash -c "$(curl -fsSL https://gitee.com/hevey88/im-select/raw/master/install_mac.sh)"',
+    enabled = vim.fn.has "mac" == 1,
+    event = "VimEnter",
+    opts = {
+      set_previous_events = {},
+    },
+  },
+  {
+    "yianwillis/vimcdoc",
+    init = function() vim.o.helplang = "cn" end,
+    lazy = true,
+    build = 'sh vimcdoc.sh -i && python3 -c "$(curl -fsSL https://gitee.com/hevey88/a4_user/raw/main/utils/vimdoc_install_mac.py)"',
+  },
+}
