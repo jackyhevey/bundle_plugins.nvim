196f272f701592b88a982d3451aeeb52564ee08f
zhjch <bingyu27@126.com>
2024-03-29 23:05:47 +0800

    update trouble

--- lua/bundle_plugins/init.lua
+++ lua/bundle_plugins/init.lua
@@ -253,37 +253,37 @@ end
 -- --- Table based API for setting keybindings
 -- ---@param map_table AstroCoreMappings A nested table where the first key is the vim mode, the second key is the key to map, and the value is the function to set the mapping to
 -- ---@param base? vim.api.keyset.keymap A base set of options to set on every keybinding
--- function M.set_mappings(map_table, base)
---   local was_no_which_key_queue = not M.which_key_queue
---   -- iterate over the first keys for each mode
---   base = vim.tbl_deep_extend("force", { silent = true }, base or {})
---   for mode, maps in pairs(map_table) do
---     -- iterate over each keybinding set in the current mode
---     for keymap, options in pairs(maps) do
---       -- build the options for the command accordingly
---       if options then
---         local cmd
---         local keymap_opts = base
---         if type(options) == "string" then
---           cmd = options
---         else
---           cmd = options[1]
---           keymap_opts = vim.tbl_deep_extend("force", keymap_opts, options)
---           keymap_opts[1] = nil
---         end
---         if not cmd or keymap_opts.name then -- if which-key mapping, queue it
---           if not keymap_opts.name then keymap_opts.name = keymap_opts.desc end
---           if not M.which_key_queue then M.which_key_queue = {} end
---           if not M.which_key_queue[mode] then M.which_key_queue[mode] = {} end
---           M.which_key_queue[mode][keymap] = keymap_opts
---         else -- if not which-key mapping, set it
---           vim.keymap.set(mode, keymap, cmd, keymap_opts)
---         end
---       end
---     end
---   end
---   if was_no_which_key_queue and M.which_key_queue then M.on_load("which-key.nvim", M.which_key_register) end
--- end
+function M.set_mappings(map_table, base)
+  local was_no_which_key_queue = not M.which_key_queue
+  -- iterate over the first keys for each mode
+  base = vim.tbl_deep_extend("force", { silent = true }, base or {})
+  for mode, maps in pairs(map_table) do
+    -- iterate over each keybinding set in the current mode
+    for keymap, options in pairs(maps) do
+      -- build the options for the command accordingly
+      if options then
+        local cmd
+        local keymap_opts = base
+        if type(options) == "string" then
+          cmd = options
+        else
+          cmd = options[1]
+          keymap_opts = vim.tbl_deep_extend("force", keymap_opts, options)
+          keymap_opts[1] = nil
+        end
+        if not cmd or keymap_opts.name then -- if which-key mapping, queue it
+          if not keymap_opts.name then keymap_opts.name = keymap_opts.desc end
+          if not M.which_key_queue then M.which_key_queue = {} end
+          if not M.which_key_queue[mode] then M.which_key_queue[mode] = {} end
+          M.which_key_queue[mode][keymap] = keymap_opts
+        else -- if not which-key mapping, set it
+          vim.keymap.set(mode, keymap, cmd, keymap_opts)
+        end
+      end
+    end
+  end
+  if was_no_which_key_queue and M.which_key_queue then M.on_load("which-key.nvim", M.which_key_register) end
+end
 --
 -- --- regex used for matching a valid URL/URI string
 -- M.url_matcher =
