9ab5671004ab40db46bb53f1e1846b45133495ac
zhjch <bingyu27@126.com>
2024-03-29 21:52:16 +0800

    update astrolsp for python

--- lua/bundle_plugins/plugins/pack-python.lua
+++ lua/bundle_plugins/plugins/pack-python.lua
@@ -1,5 +1,38 @@
 return {
   {
+    "AstroNvim/astrolsp",
+    optional = true,
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      table.insert(opts.servers, "ruff_lsp")
+      opts.config = require("astrocore").extend_tbl(opts.config or {}, {
+        ruff_lsp = {
+          filetypes = { "python" },
+          root_dir = function(...)
+            local util = require "lspconfig.util"
+            return util.find_git_ancestor(...)
+              or util.root_pattern(unpack {
+                "pyproject.toml",
+                "setup.py",
+                "setup.cfg",
+                "requirements.txt",
+                "Pipfile",
+                "pyrightconfig.json",
+              })(...)
+          end,
+          cmd = { "ruff-lsp" },
+          single_file_support = true,
+          init_options = {
+            settings = {
+              lint = { enable = false },
+              -- organizeImports = false,
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
       opts.ensure_installed = require("bundle_plugins").list_insert_unique(opts.ensure_installed, {
