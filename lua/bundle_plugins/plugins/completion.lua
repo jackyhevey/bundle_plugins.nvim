409505eb59595d428ae80842573a575f6a1e0c6c
zhjch <bingyu27@126.com>
2024-03-29 23:28:13 +0800

    update completion

--- lua/bundle_plugins/plugins/completion.lua
+++ lua/bundle_plugins/plugins/completion.lua
@@ -1,5 +1,40 @@
 return {
   {
+    "hrsh7th/cmp-cmdline",
+    keys = { ":", "/", "?" }, -- lazy load cmp on more keys along with insert mode
+    dependencies = { "hrsh7th/nvim-cmp" },
+    opts = function()
+      local cmp = require "cmp"
+      return {
+        {
+          type = "/",
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = {
+            { name = "buffer" },
+          },
+        },
+        {
+          type = ":",
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = cmp.config.sources({
+            { name = "path" },
+          }, {
+            {
+              name = "cmdline",
+              option = {
+                ignore_cmds = { "Man", "!" },
+              },
+            },
+          }),
+        },
+      }
+    end,
+    config = function(_, opts)
+      local cmp = require "cmp"
+      vim.tbl_map(function(val) cmp.setup.cmdline(val.type, val) end, opts)
+    end,
+  },
+  {
     "codota/tabnine-nvim",
     main = "tabnine",
     enabled = vim.loop.os_uname().sysname ~= "Linux",
